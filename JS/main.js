e12eed374285fcba636c7f9f34d19eebcf960af3
ieeah <lbnfnc90@gmail.com>
2021-10-25 16:59:50 +0200

    sperimentando con scarso successo

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,51 @@
 
 const board = document.querySelector(".board");
 const box = document.querySelector(".box");
+const div = document.createElement('div');
 // creare un loop che giri 100 volte
 // ad ogni loop deve inserire un div.box all'interno di board
 // a seconda del numero del box stesso, verificare che sia divisibile o meno per 3, per 5 e per entrambi, e in base a questo assegnarli una classe che ne modifichi sfondo e contenuto
-for (let i = 1; i < 101; i++) {
-    board.innerHTML += `<div class=box>${i}</div>`;
+// for (let i = 1; i < 101; i++) {
+//     board.append(div);
     
-        // if (i % 3) {
-        //     box.classList.add("fizz");
-        //     console.log("divisibile per 3");
-        // } else if (i % 5) {
-        //     box.classList.add("buzz");
-        //     console.log("divisibile per 5");
-        // } else if ((i % 3) && (i % 5)) {
-        //     box.classList.add("fizzBuzz");
-        //     console.log("divisibile per entrambi");
-        // }
+    
+// }
 
-}
+board.append(div);
+document.querySelector(".board > div").classList.add("box");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (i % 15 == 0) {
+//     document.querySelector(".box").classList.add("fizzBuzz");
+//     // console.log("divisibile per entrambi");
+// } else 
+// if (i % 5 == 0) {
+//     document.querySelector(".box").classList.add("buzz");
+//     // console.log("divisibile per 5");
+// } else if (i % 3 == 0) {
+//     document.querySelector(".box").classList.add("fizz");
+//     // console.log("divisibile per 3");
+// }
